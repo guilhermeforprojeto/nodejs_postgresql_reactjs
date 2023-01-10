9cfc470f6f1c5fd9791066408431f238341bf972
guilhermeforprojeto <guylhermesantos@hotmail.com>
2023-01-10 15:29:08 -0300

    Rota CREATE

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,24 +1,32 @@
-const { response } = require("express");
 const express = require("express")
 
 const Data = [{ nome: "Guilherme", status: false }]
 const allRoutes = express.Router()
+const { PrismaClient } = require("@prisma/client")
+
+const prisma = new PrismaClient();
 
 
 // C
-allRoutes.post("/todos", (request, response) => {
-  const { name } = request.body
-  Data.push({ name, status: false })
-  return response.status(201).json(Data)
+allRoutes.post("/todos", async (request, response) => {
+  const { name } = request.body;
+  const All = await prisma.All.create({
+    data: {
+      name,
+      status: false,
+    }
+  })
+  // Data.push({ name, status: false })
+  return response.status(201).json(All)
 });
-
-
 // R
 allRoutes.get("/todos", (request, response) => {
   return response.status(200).json(Data)
 })
 
 // U
+
+
 // D
 
 
